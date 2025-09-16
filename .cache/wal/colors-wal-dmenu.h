92edc6df5a19542444ff7e4b658ed7b5ab358da1
Henri-VS <willem@vanschaik.co.za>
2025-09-16 15:42:04 +0200

    Removed wel config

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e0edf2", "#13214a" },
-	[SchemeSel] = { "#e0edf2", "#2B72CC" },
-	[SchemeOut] = { "#e0edf2", "#C7DAEB" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#13214a";
-static const char* selfgcolor   = "#e0edf2";
-static const char* normbgcolor  = "#4298D9";
-static const char* normfgcolor  = "#e0edf2";
-static const char* urgbgcolor   = "#2B72CC";
-static const char* urgfgcolor   = "#e0edf2";
